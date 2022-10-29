45525546f9bc234790ef6f6572425b9bfe566308
Sardor0795 <english.programmer.9595@gmail.com>
2022-10-29 18:56:47 +0500

    Test element is exist or not to avoid small bugs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const elHeaderMenuBtn = document.querySelector(".header__menu-btn-wrapper");
-
 const elHeader = document.querySelector(".header");
 
-elHeaderMenuBtn.onclick = () => {
-  elHeaderMenuBtn.classList.toggle("active");
-  elHeader.classList.toggle("mobile-menu");
-  document.body.classList.toggle("overflow");
-};
+if (elHeaderMenuBtn) {
+  elHeaderMenuBtn.onclick = () => {
+    elHeaderMenuBtn.classList.toggle("active");
+    elHeader.classList.toggle("mobile-menu");
+    document.body.classList.toggle("overflow");
+  };
+}
