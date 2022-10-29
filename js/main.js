0c0195195b3d2c5ac4c603cb522dfb6101212074
Sardor0795 <english.programmer.9595@gmail.com>
2022-10-29 18:37:16 +0500

    Fix modal window bugs

--- js/main.js
+++ js/main.js
@@ -5,4 +5,5 @@ const elHeader = document.querySelector(".header");
 elHeaderMenuBtn.onclick = () => {
   elHeaderMenuBtn.classList.toggle("active");
   elHeader.classList.toggle("mobile-menu");
+  document.body.classList.toggle("overflow");
 };
